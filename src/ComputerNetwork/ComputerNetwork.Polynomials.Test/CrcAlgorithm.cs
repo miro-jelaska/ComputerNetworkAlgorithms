9118c09cb68050c98f7f376310990e3609016439
Miro Jelaska <git@jelaska.com>
2016-02-22 19:52:51 +0100

    Added first homework

---
+++ src/ComputerNetwork/ComputerNetwork.Polynomials.Test/CrcAlgorithm.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace ComputerNetwork.Polynomials.Test
+{
+    public abstract class CrcAlgorithm
+    {
+        [TestFixture]
+        public class GenerateCrc
+        {
+            [Test]
+            public void Generate()
+            {
+                var a = new Polynomials.BinaryPolynomial("10110011");
+                var crc = Polynomials.CrcAlgorithm.GenerateCrc(a);
+
+                crc.Should().Be(new Polynomials.BinaryPolynomial("101100111000"));
+            }
+        }
+
+        [TestFixture]
+        public class IsValid
+        {
+            [Test]
+            public void ShouldBeValid()
+            {
+                var crcPolynomial = new Polynomials.BinaryPolynomial("101100111000");
+                Polynomials.CrcAlgorithm.IsCrcValid(crcPolynomial).Should().BeTrue();
+            }
+
+            [Test]
+            public void ShouldBeInvalid()
+            {
+                var crcPolynomial = new Polynomials.BinaryPolynomial("101100111001");
+                Polynomials.CrcAlgorithm.IsCrcValid(crcPolynomial).Should().BeFalse();
+            }
+        }
+    }
+}
---
+++ src/ComputerNetwork/ComputerNetwork.Polynomials/DecimalMonomial.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using FluentAssertions;
+
+namespace ComputerNetwork.Polynomials
+{
+    /// <summary>
+    /// This class reprezents one part of the Polynomial.
+    /// This is not an mathematical but artificial structure which is essential building block of Polynomials.
+    /// </summary>
+    /// <example> 
+    /// In this polynomial
+    /// 4x^3 + 2x -1
+    /// monomials are [4x^3, 2x, -1]
+    /// </example>
+    public class DecimalMonomial
+    {
+        /// <summary>
+        /// Constructs monomial.
+        /// <example> 
+        /// E.g. for monomial
+        /// 4x^3
+        /// value is 4 and degree is 3
+        /// </example>
+        /// </summary>
+        public DecimalMonomial(int value, int degree)
+        {
+            this.Value = value;
+            this.Degree = degree;
+
+            degree.Should().BeGreaterOrEqualTo(1);
+        }
+
+        public DecimalMonomial() : this(0, 1)
+        {
+        }
+
+        public int Value { get; }
+        public int Degree { get; }
+
+        public static DecimalMonomial operator +(DecimalMonomial left, DecimalMonomial right)
+        {
+            (left.Degree == right.Degree).Should().BeTrue();
+            return new DecimalMonomial(left.Value + right.Value, left.Degree);
+        }
+        public static DecimalMonomial operator -(DecimalMonomial left, DecimalMonomial right)
+        {
+            (left.Degree == right.Degree).Should().BeTrue();
+            return new DecimalMonomial(left.Value - right.Value, left.Degree);
+        }
+        public static DecimalMonomial operator *(DecimalMonomial left, DecimalMonomial right)
+        {
+            return new DecimalMonomial(left.Value * right.Value, left.Degree + right.Degree);
+        }
+        public static DecimalMonomial operator /(DecimalMonomial left, DecimalMonomial right)
+        {
+            left.Degree.Should().BeGreaterThan(0);
+            right.Degree.Should().BeGreaterThan(0);
+            return new DecimalMonomial(left.Value / right.Value, left.Degree - right.Degree);
+        }
+        public static DecimalMonomial operator %(DecimalMonomial left, DecimalMonomial right)
+        {
+            return new DecimalMonomial(left.Value % right.Value, left.Degree - right.Degree);
+        }
+
+        public override string ToString()
+        {
+            var sign = this.Value < 0 ? "-" : "+";
+            var xPart = this.Degree > 1 ? $"x{this.Degree - 1}" : string.Empty;
+
+            return $"{sign} {Math.Abs(this.Value)}{xPart}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            var objAsMonomial = obj as DecimalMonomial;
+            if (objAsMonomial == null)
+                return false;
+
+            return this.Equals(objAsMonomial);
+        }
+
+        protected bool Equals(DecimalMonomial other)
+        {
+            if (other == null)
+                return false;
+
+            return this.Value == other.Value && this.Degree == other.Degree;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Value * 397) ^ Degree;
+            }
+        }
+    }
+}
